97811fd44955ec196be28f8c80196d63882b9c86
YaelCami <yaelc.calderonv@uqvirtual.edu.co>
2025-08-15 22:44:01 -0500

    init commit

---
+++ ClinicaVeterinariaPro/src/test/java/co/edu/uniquindio/poo/clinicaveterinariapro/model/SecretariaTest.java
@@ -0,0 +1,20 @@
+package co.edu.uniquindio.poo.clinicaveterinariapro.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SecretariaTest {
+
+    @Test
+    void disponibilidadCitas() {
+    }
+
+    @Test
+    void buscarVeterinarioOcupado() {
+    }
+
+    @Test
+    void disponibilidadVeterinario() {
+    }
+}
---
+++ ClinicaVeterinariaPro/src/test/java/co/edu/uniquindio/poo/clinicaveterinariapro/model/VeterinariaTest.java
@@ -0,0 +1,192 @@
+package co.edu.uniquindio.poo.clinicaveterinariapro.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VeterinariaTest {
+
+    @Test
+    void verificarPropietario() {
+    }
+
+    @Test
+    void verificarMascota() {
+    }
+
+    @Test
+    void verificarVeterinario() {
+    }
+
+    @Test
+    void verificarSecretaria() {
+    }
+
+    @Test
+    void verificarAseo() {
+    }
+
+    @Test
+    void verificarConsulta() {
+    }
+
+    @Test
+    void verificarCita() {
+    }
+
+    @Test
+    void verificarTratamiento() {
+    }
+
+    @Test
+    void eliminarPropietario() {
+    }
+
+    @Test
+    void eliminarMascota() {
+    }
+
+    @Test
+    void eliminarVeterinario() {
+    }
+
+    @Test
+    void eliminarSecretaria() {
+    }
+
+    @Test
+    void eliminarAseo() {
+    }
+
+    @Test
+    void eliminarCita() {
+    }
+
+    @Test
+    void eliminarConsulta() {
+    }
+
+    @Test
+    void eliminarTratamiento() {
+    }
+
+    @Test
+    void agregarPropietario() {
+    }
+
+    @Test
+    void agregarMascota() {
+    }
+
+    @Test
+    void agregarVeterinario() {
+    }
+
+    @Test
+    void agregarSecretaria() {
+    }
+
+    @Test
+    void agregarAseo() {
+    }
+
+    @Test
+    void agregarCita() {
+    }
+
+    @Test
+    void agregarConsulta() {
+    }
+
+    @Test
+    void agregarTratamiento() {
+    }
+
+    @Test
+    void actualizarPropietario() {
+    }
+
+    @Test
+    void actualizarMascota() {
+    }
+
+    @Test
+    void actualizarVeterinario() {
+    }
+
+    @Test
+    void actualizarSecretaria() {
+    }
+
+    @Test
+    void actualizarAseo() {
+    }
+
+    @Test
+    void actualizarCita() {
+    }
+
+    @Test
+    void actualizarConsulta() {
+    }
+
+    @Test
+    void actualizarTratamiento() {
+    }
+
+    @Test
+    void buscarPropietario() {
+    }
+
+    @Test
+    void buscarMascota() {
+    }
+
+    @Test
+    void buscarVeterinario() {
+    }
+
+    @Test
+    void buscarSecretaria() {
+    }
+
+    @Test
+    void buscarAseo() {
+    }
+
+    @Test
+    void buscarCita() {
+    }
+
+    @Test
+    void buscarConsulta() {
+    }
+
+    @Test
+    void buscarTramiento() {
+    }
+
+    @Test
+    void cantidadPerros() {
+    }
+
+    @Test
+    void cantidadGatos() {
+    }
+
+    @Test
+    void cantidadAves() {
+    }
+
+    @Test
+    void cantidadOtrasMascotas() {
+    }
+
+    @Test
+    void hallarListaCitasVeterinario() {
+    }
+
+    @Test
+    void hallarlistaConsultasMascota() {
+    }
+}
